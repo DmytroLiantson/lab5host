868ba6fed0b97a6eb9fa6e387d5f89b353adf00a
dmytr <dmytro.hrynchyshyn.oi.2023@lpnu.ua>
2025-05-27 23:06:42 +0300

    try11

--- server/server.js
+++ server/server.js
@@ -108,8 +108,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 10000;
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
