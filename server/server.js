bb363244a5ca3f0a85a48aafe36f078b1add5a68
dmytr <dmytro.hrynchyshyn.oi.2023@lpnu.ua>
2025-05-27 21:13:54 +0300

    lab5host2

--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = JSON.parse(process.env.serviceAccountKey);
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
